4505113bc45eeedbf249f138ab8d5ce5b2079ff4
chenyuqiang <chenyuqiang@luckyzune.com>
2018-05-02 04:29:41 +0800

    最终

---
+++ web-mobile/main.100fa.js
@@ -0,0 +1,169 @@
+(function () {
+
+    'use strict';
+
+    function boot() {
+
+        var settings = window._CCSettings;
+        window._CCSettings = undefined;
+
+        var uuids = settings.uuids;
+
+        var rawAssets = settings.rawAssets;
+        var assetTypes = settings.assetTypes;
+        var realRawAssets = settings.rawAssets = {};
+        for (var mount in rawAssets) {
+            var entries = rawAssets[mount];
+            var realEntries = realRawAssets[mount] = {};
+            for (var id in entries) {
+                var entry = entries[id];
+                var type = entry[1];
+                // retrieve minified raw asset
+                if (typeof type === 'number') {
+                    entry[1] = assetTypes[type];
+                }
+                // retrieve uuid
+                realEntries[uuids[id] || id] = entry;
+            }
+        }
+
+        var scenes = settings.scenes;
+        for (var i = 0; i < scenes.length; ++i) {
+            var scene = scenes[i];
+            if (typeof scene.uuid === 'number') {
+                scene.uuid = uuids[scene.uuid];
+            }
+        }
+
+        var packedAssets = settings.packedAssets;
+        for (var packId in packedAssets) {
+            var packedIds = packedAssets[packId];
+            for (var j = 0; j < packedIds.length; ++j) {
+                if (typeof packedIds[j] === 'number') {
+                    packedIds[j] = uuids[packedIds[j]];
+                }
+            }
+        }
+
+        // init engine
+        var canvas;
+
+        if (cc.sys.isBrowser) {
+            canvas = document.getElementById('GameCanvas');
+        }
+
+        function setLoadingDisplay() {
+            // Loading splash scene
+            var splash = document.getElementById('splash');
+            var progressBar = splash.querySelector('.progress-bar span');
+            cc.loader.onProgress = function (completedCount, totalCount, item) {
+                var percent = 100 * completedCount / totalCount;
+                if (progressBar) {
+                    progressBar.style.width = percent.toFixed(2) + '%';
+                }
+            };
+            splash.style.display = 'block';
+            progressBar.style.width = '0%';
+
+            cc.director.once(cc.Director.EVENT_AFTER_SCENE_LAUNCH, function () {
+                splash.style.display = 'none';
+            });
+        }
+
+        var onStart = function () {
+            cc.view.resizeWithBrowserSize(true);
+            // UC browser on many android devices have performance issue with retina display
+            if (cc.sys.os !== cc.sys.OS_ANDROID || cc.sys.browserType !== cc.sys.BROWSER_TYPE_UC) {
+                cc.view.enableRetina(true);
+            }
+            //cc.view.setDesignResolutionSize(settings.designWidth, settings.designHeight, cc.ResolutionPolicy.SHOW_ALL);
+
+            if (cc.sys.isBrowser) {
+                setLoadingDisplay();
+            }
+
+            if (cc.sys.isMobile) {
+                if (settings.orientation === 'landscape') {
+                    cc.view.setOrientation(cc.macro.ORIENTATION_LANDSCAPE);
+                }
+                else if (settings.orientation === 'portrait') {
+                    cc.view.setOrientation(cc.macro.ORIENTATION_PORTRAIT);
+                }
+                // qq, wechat, baidu
+                cc.view.enableAutoFullScreen(
+                    cc.sys.browserType !== cc.sys.BROWSER_TYPE_BAIDU &&
+                    cc.sys.browserType !== cc.sys.BROWSER_TYPE_WECHAT &&
+                    cc.sys.browserType !== cc.sys.BROWSER_TYPE_MOBILE_QQ
+                );
+            }
+
+            // init assets
+            cc.AssetLibrary.init({
+                libraryPath: 'res/import',
+                rawAssetsBase: 'res/raw-',
+                rawAssets: settings.rawAssets,
+                packedAssets: settings.packedAssets,
+                md5AssetsMap: settings.md5AssetsMap
+            });
+
+            var launchScene = settings.launchScene;
+
+            // load scene
+            if (cc.runtime) {
+                cc.director.setRuntimeLaunchScene(launchScene);
+            }
+            cc.director.loadScene(launchScene, null,
+                function () {
+                    if (cc.sys.isBrowser) {
+                        // show canvas
+                        canvas.style.visibility = '';
+                        var div = document.getElementById('GameDiv');
+                        if (div) {
+                            div.style.backgroundImage = '';
+                        }
+                    }
+                    cc.loader.onProgress = null;
+
+                    // play game
+                    // cc.game.resume();
+                    console.timeEnd('start')
+                    console.log('Success to load scene: ' + launchScene);
+                }
+            );
+        };
+
+        // jsList
+        var jsList = settings.jsList;
+
+        var option = {
+            //width: width,
+            //height: height,
+            id: 'GameCanvas',
+            scenes: settings.scenes,
+            debugMode: cc.DebugMode.ERROR,
+            showFPS: settings.debug,
+            frameRate: 60,
+            jsList: jsList,
+            groupList: settings.groupList,
+            collisionMatrix: settings.collisionMatrix,
+            renderMode: 0
+        };
+
+        cc.game.run(option, onStart);
+    }
+
+    if (window.document) {
+        var splash = document.getElementById('splash');
+        splash.style.display = 'block';
+
+        window.eruda && eruda.init() && eruda.get('console').config.set('displayUnenumerable', false);
+        boot();
+    }
+    else if (window.jsb) {
+        require('src/settings.00d3b.js');
+        require('src/jsb_polyfill.js');
+
+        boot();
+    }
+
+})();
